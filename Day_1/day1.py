a56d2511ade1d3f89ac5e1e66a8d1f50ce9d7744
Naing-Bhone-Htet <67512467+Naing-Bhone-Htet@users.noreply.github.com>
2024-06-26 18:20:47 +0800

    Add files via upload

---
+++ Day_1/day1.py
@@ -0,0 +1,22 @@
+#day 1 
+a,b = 12,13
+
+print(a + b)    #addition(+)
+print(a - b)    #subtraction(-)
+print(a * b)    #Multiplication(*) 
+print(a ** b)   # exponential(**)
+print(a / b)    # division(/)
+print(a % b)    # modulus(%)
+print(a // b)   # Floor division operator(//)
+
+# data type checking 
+
+print(type(20))                     #int
+print(type(2.20))                   #Float
+print(type("Hello World!"))         #str
+print(type(1 + 3j))                 #complex
+print(type(True))                   #bool    
+print(type([1,2,3]))                #list
+print(type({'name' :'Naing' }))     #dictionary
+print(type({2,2,3}))                #set
+print(type((2.8,2.14,3)))           #tuple
